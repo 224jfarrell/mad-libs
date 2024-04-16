4c6cc6d6a5df5fd124cd3a3174faab0333f067f5
224jfarrell <224jfarrell@frhsd.com>
2024-04-16 11:53:50 -0400

    a

---
+++ scripts.js
@@ -0,0 +1,27 @@
+let A = document.getElementById('a');
+let B = document.getElementById('b');
+let C = document.getElementById('c');
+let D = document.getElementById('d');
+let E = document.getElementById('e');
+let F = document.getElementById('f');
+let G = document.getElementById('g');
+let H = document.getElementById('h');
+let I = document.getElementById('i');
+let J = document.getElementById('j');
+let K = document.getElementById('k');
+let L, M, N, O, P, Q, R, S, T, U, V;
+let W = document.getElementById('story');
+let X = `Hey you!
+
+Guess what? Last night was an absolute blast! We ended up at this quirky little karaoke bar downtown, where the drinks were flowing and the dance floor was on fire. Picture this: me, belting out 
+'80s classics like a rock star, while the crowd cheered me on like I was headlining at Madison Square Garden. And just when I thought the night couldn't get any crazier, we stumbled upon a late-night 
+taco truck that served up the most mouthwatering carne asada tacos this side of the border. It was one of those nights where the stars aligned, and the memories were made of pure gold. Can't wait to 
+fill you in on all the juicy details!
+
+Catch you later,
+[Your Name]`;
+
+
+function submit(){
+    console.log(A.value);
+}
